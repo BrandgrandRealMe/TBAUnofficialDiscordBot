563395606882476735423384a3289d6df863529e
BrandgrandReal <BrandonTheDev@proton.me>
2024-07-28 01:12:04 +0000

    Added version in statuses

--- handlers/status.js
+++ handlers/status.js
@@ -1,17 +1,18 @@
 import { ActivityType } from "discord.js";
 import { client } from "../bot.js";
+import settings from "./settings/config.js";
 
 const statuses = [
   { text: "and staring at your robot!", type: ActivityType.Watching },
   { text: "with code.", type: ActivityType.Playing },
   { text: "with Data.", type: ActivityType.Playing },
   { text: "and Helping users ✨", type: ActivityType.Watching },
+  { text: `Running version ${settings.VER}`, type: ActivityType.Watching },
 ];
 
 let currentStatusIndex = 0; // Keeps track of the current status
 
 export function updateStatus() {
-
   // Choose the next status
   currentStatusIndex = (currentStatusIndex + 1) % statuses.length;
 
